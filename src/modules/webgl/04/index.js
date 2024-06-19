2ce8fa137275a846b658637e298643180883f8e7
杨登程 <dengcheng.yang@jimuitech.com>
2024-06-19 20:11:29 +0800

    feat: 绘制动态三角形

--- src/modules/webgl/04/index.js
+++ src/modules/webgl/04/index.js
@@ -1,11 +1,7 @@
-// 需求：绘制三角形
+// 需求：通过点击三个点绘制三角形
 
-/**
- * 三角形的三种绘制模式：基本三角形、三角带、三角扇
- */
-
-import frag from "@/modules/webgl/03/index.frag"
-import vert from "@/modules/webgl/03/index.vert"
+import frag from "@/modules/webgl/04/index.frag"
+import vert from "@/modules/webgl/04/index.vert"
 import { createProgram, createShader } from "@/utils"
 
 const myCanvas = document.querySelector("#myCanvas")
@@ -31,11 +27,17 @@ const program = createProgram(gl, vertexShader, fragmentShader)
 // 使用着色器程序对象
 gl.useProgram(program)
 
-// 定义三个顶点
-const point = [1.0, 0.5, 0.2, 0.3, 0.6, 0.8]
+// 定义三个顶点，传入 canvas 的 x 和 y 坐标
+const point = []
 
 // 读取 position 地址
 const a_position = gl.getAttribLocation(program, "a_position")
+
+// 读取 canvas 的宽高
+const a_screen_size = gl.getAttribLocation(program, "a_screen_size")
+
+// 给宽高赋值
+gl.vertexAttrib2f(a_screen_size, myCanvas.width, myCanvas.height)
 
 // 创建缓冲区
 const buffer = gl.createBuffer()
@@ -43,11 +45,6 @@ const buffer = gl.createBuffer()
 // 绑定缓冲区
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
 
-// 将数据写入缓冲区
-// webgl 浮点数占用 4 个字节，32 位
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(point), gl.STATIC_DRAW)
-
-// 将缓冲区的数据传入到顶点着色器
 
 // 将缓冲区的数据和 a_position 关联
 gl.enableVertexAttribArray(a_position)
@@ -60,8 +57,20 @@ const offset = 0 // 从第一个数据开始取
 
 gl.vertexAttribPointer(a_position, size, type, normalize, stride, offset)
 
+myCanvas.addEventListener("click", (e) => {
+    const x = e.pageX;
+    const y = e.pageY;
+    point.push(x, y);
+
+    if (point.length % 6 === 0) {
+        // 将数据写入缓冲区
+        // webgl 浮点数占用 4 个字节，32 位
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(point), gl.STATIC_DRAW)
+        gl.clear(gl.COLOR_BUFFER_BIT)
+        gl.drawArrays(gl.TRIANGLES, 0, point.length / 2)
+    }
+})
+
 gl.clearColor(0.0, 0.0, 0.0, 1.0)
 gl.clear(gl.COLOR_BUFFER_BIT)
 
-// 绘制三角形
-gl.drawArrays(gl.TRIANGLES, 0, 3)
